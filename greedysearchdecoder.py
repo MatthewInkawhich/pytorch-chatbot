672f94467f265a22afe36b995776f042c69f7701
Matthew Inkawhich <minkawhich@fb.com>
2018-09-09 13:53:42 -0400

    changed to scripted version of GSD module

--- greedysearchdecoder.py
+++ greedysearchdecoder.py
@@ -1,20 +1,25 @@
-class GreedySearchDecoder(nn.Module):
-    def __init__(self, encoder, decoder):
+class GreedySearchDecoder(torch.jit.ScriptModule):
+    def __init__(self, encoder, decoder, decoder_n_layers):
         super(GreedySearchDecoder, self).__init__()
         self.encoder = encoder
         self.decoder = decoder
+        self._device = device
+        self._SOS_token = SOS_token
+        self._decoder_n_layers = decoder_n_layers
 
-    def forward(self, input_seq, input_length, max_length):
+    __constants__ = ['_device', '_SOS_token', '_decoder_n_layers']
+
+    @torch.jit.script_method
+    def forward(self, input_seq : torch.Tensor, input_length : torch.Tensor, max_length : int):
         # Forward input through encoder model
         encoder_outputs, encoder_hidden = self.encoder(input_seq, input_length)
         # Prepare encoder's final hidden layer to be first hidden input to the decoder
-        decoder_hidden = encoder_hidden[:decoder.n_layers]
+        decoder_hidden = encoder_hidden[:self._decoder_n_layers]
         # Initialize decoder input with SOS_token
-        decoder_input = torch.LongTensor([[SOS_token]])
-        decoder_input = decoder_input.to(device)
+        decoder_input = torch.ones(1, 1, device=self._device, dtype=torch.long) * self._SOS_token
         # Initialize tensors to append decoded words to
-        all_tokens = torch.cat((all_tokens, decoder_input), dim=0)
-        all_scores = torch.cat((all_scores, decoder_scores), dim=0)
+        all_tokens = torch.zeros([0], device=self._device, dtype=torch.long)
+        all_scores = torch.zeros([0], device=self._device)
         # Iteratively decode one word token at a time
         for _ in range(max_length):
             # Forward pass through decoder
